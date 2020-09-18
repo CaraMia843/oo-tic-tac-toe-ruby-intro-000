58e036ac83301060872b625fee315f0b2ce34d51
Cara Zellner <carajeanine@gmail.com>
2020-09-18 17:22:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,5 +59,16 @@ WIN_COMBINATIONS = [
     turn_count % 2 == 0 ? "X" : "O"
   end
   
+  def turn(board)
+    puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
   
+end
 end
